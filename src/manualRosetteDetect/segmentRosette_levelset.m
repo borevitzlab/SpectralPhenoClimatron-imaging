4876d1cf64a30b5d74ef90ce060481162e0ddbaf
Joel Andres Granados <joel.granados@gmail.com>
2014-03-18 04:05:40 +0100

    Adjust contour levelset values.
    We are adjusting maxiter, growth and perimeter proportion of error. The most
    important here being growth as it will fail with a 'big' growth (no contours
    are found) but it will also fail with a 'small' growth as it will not get the
    growth of the plant.
    * src/manualRosetteDetect/segmentRosette_levelset.m(segmentRosette_levelset):
    We change the way we calculate the growth. Now it will be 5 or less. We need
    to put this variable as a method argument.
    (segmentRosette_levelset): We are just doing a mask swelling instead of a
    swelling and a dillation. This give a better starting point for the contour
    algorithm in the next image.
    (segmentRosette_levelset): We throw an error if the mask we detect has more
    than 10% 1's in the perimeter.
    (activeContours): We calculate all smoothing variables outside the loop.
    (activeContours): We use imgradient instead of gradient. It make code more
    readable.

--- src/manualRosetteDetect/segmentRosette_levelset.m
+++ src/manualRosetteDetect/segmentRosette_levelset.m
@@ -5,47 +5,46 @@
 function [retMask, maskRange] = segmentRosette_levelset ( imgR, img, mask )
     % true when we are satified with the mask
     foundRosette = false;
-
     maskRange = imgR;
 
-    % 1. Calculate the maximum growth in any direction.
-    growth = min ( [ imgR.yFrom imgR.xFrom ...
-                        abs(size(img,2) - imgR.xTo) ...
-                        abs(size(img,1) - imgR.yTo) ] );
-    growth = growth - mod(growth,5); % Next multiple of 5 down.
-    if ( growth > 30 ) % Assume growth is max 30 pixels in any dir.
-        growth = 30;
-    elseif ( growth < 5 )
+    % Calculate the maximum growth in any direction.
+    growth = 5;
+    maxGrowth =  min ( [ imgR.yFrom imgR.xFrom ...
+                         abs(size(img,2) - imgR.xTo) ...
+                         abs(size(img,1) - imgR.yTo) ] );
+    if ( growth > maxGrowth )
         err = MException( 'segmentRosette_levelset:InvalidMaxGrowth', ...
                           'growth var was calculated to be less than 5' );
         throw(err);
     end
 
-    % 2. Analyze increasing masks
-    maskRange = struct ( 'yFrom', imgR.yFrom-growth, 'yTo', imgR.yTo+growth, ...
-                        'xFrom', imgR.xFrom-growth, 'xTo', imgR.xTo+growth );
-
-    % 3. Dilate original mask.
-    % Swells the original mask and puts mask in dilmask.
-    % se is twice i (aprox) so dilation almost reaches size(dilmask)
-    dilmask = zeros( size(mask) + (growth*2) );
-    dilmask( growth:growth+size(mask,1)-1, ...
+    % Swells the original mask and puts mask in smask.
+    smask = zeros( size(mask) + (growth*2) );
+    smask( growth:growth+size(mask,1)-1, ...
              growth:growth+size(mask,2)-1) = mask;
-    se = strel('square', growth);
-    dilmask = imdilate(dilmask, se);
 
-    % Get features from image of dilmask's size.
+    % Get features from image of smask's size.
+    maskRange = struct ( 'yFrom', imgR.yFrom-growth, 'yTo', imgR.yTo+growth, ...
+                         'xFrom', imgR.xFrom-growth, 'xTo', imgR.xTo+growth );
     F = getFeatures ( img ( int64(maskRange.yFrom:maskRange.yTo), ...
                             int64(maskRange.xFrom:maskRange.xTo), ...
                             : ) );
+    F(:,:,1) = F(:,:,1)*(-1); % Make a* positive for green.
 
-    retMask = activeContours ( F, dilmask );
+    retMask = activeContours ( F, smask );
 
-    % 6. Remove noise and bring close connected components together.
-    retMask = imclose(retMask, strel('disk', 3) );
+    % We error when more than 10% of perimeter pixels are 1.
+    perim1 = sum(retMask(:,1)) + sum(retMask(1,:))...
+             + sum(retMask(size(retMask,1),:)) ...
+             + sum(retMask(:,size(retMask,2)));
+    perimT = 2*size(retMask,1) + 2*size(retMask,2);
+    if ( perim1/perimT > 0.10 )
+        err = MException( 'segmentRosette_levelset:ToMuchPerimeter', ...
+                          'Segmentation has been unsuccessful' );
+        throw(err);
+    end
 
-
-    % 8. Recalculate enclosing square.
+    % Recalculate enclosing square.
     cc = bwconncomp(retMask, 4);
     pixList = regionprops(cc, 'PixelList');
     pl = vertcat(pixList.PixelList);
@@ -72,14 +71,19 @@ function mask = activeContours ( Img, contMask )
     Img = reshape ( Img, size(Img,1)*size(Img,2), numDims );
     Img = double(Img);
 
+    Sigma = 1.5;
+    KernelSize = 2 * round(2 * Sigma) + 1;
+    G = fspecial('gaussian', KernelSize, Sigma);
     contMask = (contMask > 0) - (contMask <= 0);
+    contMask = conv2(contMask, G, 'same');
 
     prevContMask = zeros(size(contMask));
-    %FIXME: 30 should be a method argument.
-    for ( j = 1:30 )
+    %FIXME: maxiter should be an argument
+    for ( j = 1:250 )
         if ( j > 1 && isequal(prevContMask, contMask) )
             break;
         end
+        prevContMask = contMask;
 
         % Indeces
         ind.pos = reshape ( contMask >  0, prod(size(contMask)), 1 );
@@ -101,13 +105,9 @@ function mask = activeContours ( Img, contMask )
         term.neg = sum(term.neg, 2) / numDims;
         term.neg = reshape ( term.neg, ImgRows, ImgCols );
 
+        [Gmag, ~] = imgradient(contMask);
+        contMask = contMask + ((-term.pos + term.neg) .* Gmag);
 
-        [ux, uy] = gradient(contMask);
-        contMask = contMask + ((-term.pos + term.neg) .* sqrt(ux.^2 + uy.^2));
-
-        Sigma = 1.5;
-        KernelSize = 2 * round(2 * Sigma) + 1;
-        G = fspecial('gaussian', KernelSize, Sigma);
         contMask = (contMask >= 0) - (contMask < 0);
         contMask = conv2(contMask, G, 'same');
     end
